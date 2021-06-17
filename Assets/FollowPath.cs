3b8a4d220e810ab25682ffbf5c4ec06abe3acfe7
nightmarejv <41236857+nightmarejv@users.noreply.github.com>
2021-06-17 19:43:31 -0300

    Initial commit

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+
+    Transform goal;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+                                                                                                        //Faz a importação do navmesh
+    UnityEngine.AI.NavMeshAgent agent;
+    void Start()
+    {
+                                                                                                        //puxa as informações do wpmanager para o wps
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+                                                                                                        //Puxa o componente do agent para o tank ter movimento
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    
+
+    public void GoToHeli()
+    {
+                                                                                                        //Cria um destino para o tank ir até o heli
+        agent.SetDestination(wps[9].transform.position);
+    }
+
+    
+
+
+
+    public void GoToRuin()
+    {
+                                                                                                        //Cria um destino para o tank ir até a ruina
+        agent.SetDestination(wps[5].transform.position);      
+    }
+    
+
+
+
+    public void GoToFabrica()
+    {
+                                                                                                        //Cria um destino para o tank ir até a fabrica
+        agent.SetDestination(wps[10].transform.position);
+    }
+
+  
+}
---
+++ Assets/WPManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public struct Link
+{
+   
+    public enum direction { UNI, BI }
+
+    public GameObject node1;
+
+    public GameObject node2;
+
+    public direction dir;
+}
+
+
+
+
+public class WPManager : MonoBehaviour
+{
+                                                                                        //Cria as variaveis que serao usadas
+    public GameObject[] waypoints;
+
+
+    public Link[] links;
+
+
+    public Graph graph = new Graph();
+
+
+    void Start()
+    {
+                                                                                        //Caso o array waypoints for maior que 0, é adcionado um node  
+        if (waypoints.Length > 0)
+        {
+         
+            foreach (GameObject wp in waypoints)
+            {
+              
+                graph.AddNode(wp);
+            }
+
+                                                                                       //Para cada link, adciona um edge para o graphs  
+            foreach (Link l in links)
+            {
+               
+                graph.AddEdge(l.node1, l.node2);
+            
+                if (l.dir == Link.direction.BI)
+                  
+                    graph.AddEdge(l.node2, l.node1);
+            }
+        }
+    }
+                                                                                       //Cria um caminho de acordo com os pontos
+    void Update()
+    {
+        graph.debugDraw();
+    }
+}
+
